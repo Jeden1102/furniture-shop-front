365bfc5348d1d81ace591ba3b963ba64eb541874
Dominik Raducki <dominik.raducki@smartbees.co>
2024-10-13 15:57:08 +0200

    master. Deploy debugging.

--- src/middleware.ts
+++ src/middleware.ts
@@ -26,6 +26,8 @@ async function getRegionMap() {
     }).then((res) => res.json())
 
     if (!regions) {
+      console.log(regions);
+      console.log("HERE", `${BACKEND_URL}/store/regions`)
       notFound()
     }
 
@@ -84,58 +86,57 @@ async function getCountryCode(
  * Middleware to handle region selection and onboarding status.
  */
 export async function middleware(request: NextRequest) {
-  return NextResponse.next();
-  // const searchParams = request.nextUrl.searchParams
-  // const isOnboarding = searchParams.get("onboarding") === "true"
-  // const cartId = searchParams.get("cart_id")
-  // const checkoutStep = searchParams.get("step")
-  // const onboardingCookie = request.cookies.get("_medusa_onboarding")
-  // const cartIdCookie = request.cookies.get("_medusa_cart_id")
+  const searchParams = request.nextUrl.searchParams
+  const isOnboarding = searchParams.get("onboarding") === "true"
+  const cartId = searchParams.get("cart_id")
+  const checkoutStep = searchParams.get("step")
+  const onboardingCookie = request.cookies.get("_medusa_onboarding")
+  const cartIdCookie = request.cookies.get("_medusa_cart_id")
 
-  // const regionMap = await getRegionMap()
+  const regionMap = await getRegionMap()
 
-  // const countryCode = regionMap && (await getCountryCode(request, regionMap))
+  const countryCode = regionMap && (await getCountryCode(request, regionMap))
 
-  // const urlHasCountryCode =
-  //   countryCode && request.nextUrl.pathname.split("/")[1].includes(countryCode)
+  const urlHasCountryCode =
+    countryCode && request.nextUrl.pathname.split("/")[1].includes(countryCode)
 
-  // // check if one of the country codes is in the url
-  // if (
-  //   urlHasCountryCode &&
-  //   (!isOnboarding || onboardingCookie) &&
-  //   (!cartId || cartIdCookie)
-  // ) {
-  //   return NextResponse.next()
-  // }
+  // check if one of the country codes is in the url
+  if (
+    urlHasCountryCode &&
+    (!isOnboarding || onboardingCookie) &&
+    (!cartId || cartIdCookie)
+  ) {
+    return NextResponse.next()
+  }
 
-  // const redirectPath =
-  //   request.nextUrl.pathname === "/" ? "" : request.nextUrl.pathname
+  const redirectPath =
+    request.nextUrl.pathname === "/" ? "" : request.nextUrl.pathname
 
-  // const queryString = request.nextUrl.search ? request.nextUrl.search : ""
+  const queryString = request.nextUrl.search ? request.nextUrl.search : ""
 
-  // let redirectUrl = request.nextUrl.href
+  let redirectUrl = request.nextUrl.href
 
-  // let response = NextResponse.redirect(redirectUrl, 307)
+  let response = NextResponse.redirect(redirectUrl, 307)
 
-  // // If no country code is set, we redirect to the relevant region.
-  // if (!urlHasCountryCode && countryCode) {
-  //   redirectUrl = `${request.nextUrl.origin}/${countryCode}${redirectPath}${queryString}`
-  //   response = NextResponse.redirect(`${redirectUrl}`, 307)
-  // }
+  // If no country code is set, we redirect to the relevant region.
+  if (!urlHasCountryCode && countryCode) {
+    redirectUrl = `${request.nextUrl.origin}/${countryCode}${redirectPath}${queryString}`
+    response = NextResponse.redirect(`${redirectUrl}`, 307)
+  }
 
-  // // If a cart_id is in the params, we set it as a cookie and redirect to the address step.
-  // if (cartId && !checkoutStep) {
-  //   redirectUrl = `${redirectUrl}&step=address`
-  //   response = NextResponse.redirect(`${redirectUrl}`, 307)
-  //   response.cookies.set("_medusa_cart_id", cartId, { maxAge: 60 * 60 * 24 })
-  // }
+  // If a cart_id is in the params, we set it as a cookie and redirect to the address step.
+  if (cartId && !checkoutStep) {
+    redirectUrl = `${redirectUrl}&step=address`
+    response = NextResponse.redirect(`${redirectUrl}`, 307)
+    response.cookies.set("_medusa_cart_id", cartId, { maxAge: 60 * 60 * 24 })
+  }
 
-  // // Set a cookie to indicate that we're onboarding. This is used to show the onboarding flow.
-  // if (isOnboarding) {
-  //   response.cookies.set("_medusa_onboarding", "true", { maxAge: 60 * 60 * 24 })
-  // }
+  // Set a cookie to indicate that we're onboarding. This is used to show the onboarding flow.
+  if (isOnboarding) {
+    response.cookies.set("_medusa_onboarding", "true", { maxAge: 60 * 60 * 24 })
+  }
 
-  // return response
+  return response
 }
 
 export const config = {
