c5dfcd835f149907f5b1a70357091c34bee1b59f
Jeden1102 <bum123@o2.pl>
2024-10-20 11:35:33 +0200

    master. Fetching error fix.

--- src/modules/home/components/hero/index.tsx
+++ src/modules/home/components/hero/index.tsx
@@ -1,16 +1,24 @@
-import { Github } from "@medusajs/icons"
-import { Button, Heading } from "@medusajs/ui"
 import HeroSlider from "../hero-slider"
 import HeroOverlayBox from "../hero-overlay-box"
 import { createApolloClient } from "@lib/apolloClient"
 import { HOMEPAGE_SLIDES } from "@lib/strapi/queries"
 
-const Hero = async () => {
+const getSlides = async () => {
   const client = createApolloClient()
+  let slides = []
 
-  const { data } = await client.query({
-    query: HOMEPAGE_SLIDES,
-  })
+  try {
+    const { data } = await client.query({
+      query: HOMEPAGE_SLIDES,
+    })
+
+    slides = data
+  } catch (err) {}
+
+  return slides
+}
+const Hero = async () => {
+  const data = await getSlides()
 
   return (
     <div className="h-[95vh] md:h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle bg-gradient-to-r from-primary/20 to-white">
