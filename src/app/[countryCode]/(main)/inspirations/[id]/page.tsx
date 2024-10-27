2aaf6fbeda1174c4c01682c716e981b6b9fe954a
Jeden1102 <bum123@o2.pl>
2024-10-27 14:23:22 +0100

    master. Inspiration page image added.

--- src/app/[countryCode]/(main)/inspirations/[id]/page.tsx
+++ src/app/[countryCode]/(main)/inspirations/[id]/page.tsx
@@ -3,11 +3,12 @@ import { notFound } from "next/navigation"
 
 import { createApolloClient } from "@lib/apolloClient"
 import { GET_INSPIRATION_BY_ID } from "@lib/strapi/queries"
-import BlogTeaserBig from "@modules/blog/components/blog-teaser-big"
 import { getStrapiImgUri } from "@lib/util/strapi-img-uri"
 import Share from "../../share"
 import { Container } from "@medusajs/ui"
 import InspirationsRecent from "@modules/inspirations/components/inspirations-recent"
+import type { Inspiration } from "@modules/inspirations/components/inspirations-slider"
+import Image from "next/image"
 
 type Props = {
   params: { id: string }
@@ -33,21 +34,9 @@ export async function generateMetadata({ params }: Props): Promise<Metadata> {
   }
 }
 
-type Article = {
-  url: string
-  title: string
-  content: string
-  publishedAt: string
-  documentId: string
-  image: {
-    url: string
-    alternativeText: string
-  }
-}
-
 const getInspirationById = async (id: string) => {
   const client = createApolloClient()
-  let article: Article | null = null
+  let article: Inspiration | null = null
 
   try {
     const { data } = await client.query({
@@ -67,7 +56,15 @@ export default async function InspirationPage({ params }: Props) {
 
   return (
     <div className="content-container-narrow py-20 flex gap-12 flex-col md:flex-row w-full justify-between">
-      <div className="flex flex-col gap-20">Here inspiration</div>
+      <div className="flex flex-col gap-20">
+        <Image
+          src={getStrapiImgUri(inspiration.image.url)}
+          alt={inspiration.image.alternativeText}
+          width={900}
+          height={600}
+          className="w-full h-auto"
+        />
+      </div>
       <div className="flex flex-col gap-8">
         <Container>
           <p className="font-medium">Share this inspiration!</p>
