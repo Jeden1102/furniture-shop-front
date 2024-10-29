d9df61671c4aac31f44ce363091b46033cf33bf9
Jeden1102 <bum123@o2.pl>
2024-10-29 17:19:06 +0100

    master. 404 page styling.

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -11,19 +11,15 @@ export const metadata: Metadata = {
 export default function NotFound() {
   return (
     <div className="flex flex-col gap-4 items-center justify-center min-h-[calc(100vh-64px)]">
+      <p className="text-primary font-semibold text-5xl">404</p>
+      <p>Ooopsss...</p>
       <h1 className="text-2xl-semi text-ui-fg-base">Page not found</h1>
       <p className="text-small-regular text-ui-fg-base">
         The page you tried to access does not exist.
       </p>
-      <Link
-        className="flex gap-x-1 items-center group"
-        href="/"
-      >
-        <Text className="text-ui-fg-interactive">Go to frontpage</Text>
-        <ArrowUpRightMini
-          className="group-hover:rotate-45 ease-in-out duration-150"
-          color="var(--fg-interactive)"
-        />
+      <Link className="flex gap-x-1 items-center group" href="/">
+        <Text className="text-primary font-semibold">Go to frontpage</Text>
+        <ArrowUpRightMini className="group-hover:rotate-45 ease-in-out duration-150 text-primary" />
       </Link>
     </div>
   )
